8bcdff135b30c2a7f5a2682ced6507a49e480b59
atxrenegade <harleighabel@gmail.com>
2017-01-12 00:26:37 -0700

    test_commit

---
+++ Sample.c
@@ -0,0 +1,13 @@
+/*****This is a test set up from linux Ubuntu
+ to my git hub repository****/
+
+// Harleigh Abel
+// Jan 12, 2017
+
+#include <stdio.h>
+
+int main()
+{
+printf("hello, world");
+return 0;
+}
